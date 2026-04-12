2ec691c82669c254966c6a9e865bd9cee753dece
sulikk <barteksulecki@gmail.com>
2026-04-13 00:49:38 +0200

    lokacje endpoint oraz serwis

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,7 @@ from app.core.database import engine, Base
 from app.models import user
 from app.api import user_routes
 from app.api import event_routes
+from app.api import location_routes
 from app.models import user, event, expense, location, message
 
 Base.metadata.create_all(bind=engine)
@@ -30,6 +31,8 @@ app.add_middleware(
 )
 app.include_router(user_routes.router)
 app.include_router(event_routes.router)
+app.include_router(location_routes.router)
+
 @app.get("/")
 async def root():
     return {"message": "Witaj w FriendSync API! Serwer działa poprawnie."}
