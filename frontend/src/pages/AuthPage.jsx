e0b9fa1274430bf97319bce407be8f7ad858bf27
addrumer <adam.mlotkowski12@gmail.com>
2026-05-05 13:25:39 +0200

    Update AuthPage.jsx

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const API_URL = "http://127.0.0.1:8000/api/users";
+const API_URL = (import.meta.env.VITE_API_URL || "http://127.0.0.1:8000") + "/api/users";
 
 export default function AuthPage() {
   const [isLogin, setIsLogin] = useState(true);
